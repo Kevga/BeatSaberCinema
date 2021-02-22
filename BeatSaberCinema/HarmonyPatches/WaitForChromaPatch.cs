54fc04144501027c1d9757e5b226fb121d18fcf2
Kevin Galitzki <dakari91@gmail.com>
2021-02-22 16:21:01 +0100

    Workaround for CustomPlatforms un-doing some environment modifications

--- BeatSaberCinema/HarmonyPatches/WaitForChromaPatch.cs
+++ BeatSaberCinema/HarmonyPatches/WaitForChromaPatch.cs
@@ -23,6 +23,11 @@ namespace BeatSaberCinema
 			//If we run before Chroma, the prop groups will get different IDs than usual due to the changed z-positions.
 			yield return new WaitForEndOfFrame();
 			yield return new WaitForEndOfFrame();
+
+			//Turns out CustomPlatforms runs even later and undoes some of the scene modifications Cinema does. Waiting for a specific duration is more of a temporary fix.
+			//TODO Find a better way to implement this. The problematic coroutine in CustomPlatforms is CustomFloorPlugin.EnvironmentHider+<InternalHideObjectsForPlatform>
+			yield return new WaitForSeconds(0.3f);
+
 			EnvironmentController.ModifyGameScene(PlaybackController.Instance.VideoConfig);
 		}
 	}
