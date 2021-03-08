44d3dd429608dd40792af21a96d6bd37c1c5f1f3
Kevin Galitzki <dakari91@gmail.com>
2021-03-08 12:54:28 +0100

    Fix exception when pausing DiSounds music player

--- BeatSaberCinema/HarmonyPatches/SongPreviewPatch.cs
+++ BeatSaberCinema/HarmonyPatches/SongPreviewPatch.cs
@@ -14,6 +14,18 @@ namespace BeatSaberCinema
 		public static void Postfix(SongPreviewPlayer __instance, int ____activeChannel, float ____timeToDefaultAudioTransition, AudioSource[] ____audioSources,
 			AudioClip audioClip, float startTime, float duration)
 		{
+			if (audioClip == null)
+			{
+				Log.Debug("SongPreviewPlayer AudioClip was null");
+				return;
+			}
+
+			if (____activeChannel < 0 || ____activeChannel > (____audioSources.Length-1))
+			{
+				Log.Debug($"No SongPreviewPlayer audio channel active ({____activeChannel})");
+				return;
+			}
+
 			if (audioClip.name == "LevelCleared")
 			{
 				Log.Debug($"Ignoring {audioClip.name} sound");
