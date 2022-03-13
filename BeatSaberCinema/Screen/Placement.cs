8eacce8d15ba92d0bad9816165cca198fdb0ef71
Kevga <dakari91@gmail.com>
2022-03-13 02:15:56 +0100

    Fix environment-specific screen placements not applying after a hot reload. Closes #44

--- BeatSaberCinema/Screen/Placement.cs
+++ BeatSaberCinema/Screen/Placement.cs
@@ -12,13 +12,13 @@ namespace BeatSaberCinema
 		public bool? CurveYAxis;
 		public int? Subsurfaces;
 
-		public static Placement SoloGameplayPlacement => new Placement(
+		private static Placement SoloGameplayPlacement => new Placement(
 			new Vector3(0, 12.4f, 67.8f),
 			new Vector3(-8, 0, 0),
 			25f
 		);
 
-		public static Placement MultiplayerPlacement => new Placement(
+		private static Placement MultiplayerPlacement => new Placement(
 			new Vector3(0, 5f, 67f),
 			new Vector3(-5, 0, 0),
 			17f
@@ -36,34 +36,61 @@ namespace BeatSaberCinema
 			12f
 		);
 
-		public Placement(Vector3 position, Vector3 rotation, float height, float? width = null)
+		private Placement(Vector3 position, Vector3 rotation, float height, float? width = null, float? curvature = null)
 		{
 			Position = position;
 			Rotation = rotation;
 			Height = height;
 			Width = width ?? (height * (16f / 9f));
+			Curvature = curvature;
 		}
 
-		public Placement(VideoConfig? config, PlaybackController.Scene scene, float aspectRatio)
+		public static Placement CreatePlacementForConfig(VideoConfig? config, PlaybackController.Scene scene, float aspectRatio)
 		{
 			var defaultPlacement = GetDefaultPlacementForScene(scene);
-			Position = config?.screenPosition ?? defaultPlacement.Position;
-			Rotation = config?.screenRotation ?? defaultPlacement.Rotation;
-			Height = config?.screenHeight ?? defaultPlacement.Height;
-			Width = Height * aspectRatio;
-			Curvature = config?.screenCurvature;
-			Subsurfaces = config?.screenSubsurfaces;
-			CurveYAxis = config?.curveYAxis;
+			if (scene == PlaybackController.Scene.MultiplayerGameplay || config == null)
+			{
+				return defaultPlacement;
+			}
+
+			var placement = new Placement(
+				config.screenPosition ?? defaultPlacement.Position,
+				config.screenRotation ?? defaultPlacement.Rotation,
+				config.screenHeight ?? defaultPlacement.Height
+			);
+
+			placement.Width = placement.Height * aspectRatio;
+			placement.Curvature = config?.screenCurvature;
+			placement.Subsurfaces = config?.screenSubsurfaces;
+			placement.CurveYAxis = config?.curveYAxis;
+
+			return placement;
 		}
 
 		public static Placement GetDefaultPlacementForScene(PlaybackController.Scene scene)
 		{
 			return scene switch
 			{
-				PlaybackController.Scene.SoloGameplay => SoloGameplayPlacement,
+				PlaybackController.Scene.SoloGameplay => (GetDefaultEnvironmentPlacement() ?? SoloGameplayPlacement),
 				PlaybackController.Scene.MultiplayerGameplay => MultiplayerPlacement,
 				PlaybackController.Scene.Menu => MenuPlacement,
 				_ => SoloGameplayPlacement
+			};
+		}
+
+		private static Placement? GetDefaultEnvironmentPlacement()
+		{
+			return Util.GetEnvironmentName() switch
+			{
+				"LinkinParkEnvironment" => new Placement(new Vector3(0f, 6.2f, 52.7f), Vector3.zero, 16f, null, 0f),
+				"BTSEnvironment" => new Placement(new Vector3(0, 12.4f, 80f), new Vector3(-8, 0, 0), 25f),
+				"KaleidoscopeEnvironment" => new Placement(new Vector3(0f, 1f, 35f), Vector3.zero, 12f),
+				"InterscopeEnvironment" => new Placement(new Vector3(0f, 6.3f, 37f), Vector3.zero, 12.5f),
+				"CrabRaveEnvironment" => new Placement(new Vector3(0f, 5.46f, 40f), new Vector3(-5f, 0f, 0f), 13f),
+				"MonstercatEnvironment" => new Placement(new Vector3(0f, 5.46f, 40f), new Vector3(-5f, 0f, 0f), 13f),
+				"SkrillexEnvironment" => new Placement(new Vector3(0f, 1.5f, 30f), Vector3.zero, 12f),
+				"WeaveEnvironment" => new Placement(new Vector3(0f, 1.5f, 21f), Vector3.zero, 4.3f, null, 0f),
+				_ => null
 			};
 		}
 	}
